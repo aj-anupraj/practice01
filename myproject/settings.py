5fdbf6d6b38ef39c644e60c52bcc6ed727b71247
aj-anupraj <anupraj.debata@gmail.com>
2023-11-22 15:28:09 +0530

    eddewd

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t1ysnwpdw*&mu$g8zb0tkkhk&g^jy6h2^8bpg@o7a0n6+fexa&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['anuprajdemo.azurewebsites.net','anuprajdemo-stagging.azurewebsites.net']
+ALLOWED_HOSTS = ['anuprajdemo-stagging.azurewebsites.net']
 
 
 # Application definition
